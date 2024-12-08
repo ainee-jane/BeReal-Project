c0fc86722b19a236658c69e3bb67d96e5e9edbd3
ainee-jane <a.voss@campus.lmu.de>
2024-12-09 00:12:19 +0100

    Update flask_server.py

--- flask_server.py
+++ flask_server.py
@@ -66,5 +66,6 @@ def track_active_day():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+
 
