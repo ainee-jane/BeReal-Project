83fcbf573917b931469f464ad2eb5c3aedd7229c
ainee-jane <a.voss@campus.lmu.de>
2024-12-09 04:25:55 +0100

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -74,16 +74,16 @@ async def group_selection(update: Update, context: ContextTypes.DEFAULT_TYPE):
         # Nachricht und Buttons je nach Auswahl
         if group == "bereal":
             text = (
-                f"✅ **Thank you! You are registered as a BeReal User. Your Participant-ID is: {chat_id} **\n\n"
-                f"💡Please answer a few initial questions: https://migroup.qualtrics.com/jfe/form/SV_0wk9mk29bMFlI6G\n\n\n"
+                f"✅ Thank you! You are registered as a BeReal User. Your Participant-ID is: {chat_id} \n\n"
+                f"💡 Please answer a few initial questions: https://migroup.qualtrics.com/jfe/form/SV_0wk9mk29bMFlI6G \n\n"
                 "💌 After a BeReal moment, you'll get a survey link with short questions.\n\n"
                 "📅 Participation ends after 14 active days. A day is 'active' if at least one relevant interaction is reported.\n\n"
                 "➕ Use /new to submit additional entries when posting a BeLate."
             )
         elif group == "bystander":
             text = (
-                f"✅ **Thank you! You are registered as a Bystander. Your Participant-ID is: {chat_id} **\n\n"
-                f"💡Please answer a few initial questions: https://migroup.qualtrics.com/jfe/form/SV_0wk9mk29bMFlI6G\n\n\n"
+                f"✅ Thank you! You are registered as a Bystander. Your Participant-ID is: {chat_id} \n\n"
+                f"💡 Please answer a few initial questions: https://migroup.qualtrics.com/jfe/form/SV_0wk9mk29bMFlI6G \n\n"
                 "💌️ After a BeReal moment, you'll get a survey link with short questions.\n\n"
                 "🚫 Ignore notifications if you haven't experienced a Bereal moment.\n\n"
                 "📅 Participation ends after 14 active days. A day is 'active' if at least one relevant interaction is reported.\n\n"
